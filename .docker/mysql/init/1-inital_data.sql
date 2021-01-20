25ac7ce9bb3022724f52f617453f64ae96b04d95
Bennet Breitfeld <bennetbreitfeld@googlemail.com>
2021-01-20 14:35:23 +0100

    MySQL: Add more inital data
    This commit provides a basic set of transactions when the database
    is (re-)created from scratch.

--- .docker/mysql/init/1-inital_data.sql
+++ .docker/mysql/init/1-inital_data.sql
@@ -1,8 +1,20 @@
-INSERT INTO accounts (firstname, lastname, balance) VALUES
+INSERT INTO accounts (firstname, lastname, balance)
+VALUES
 ('Elon', 'Musk', 15560),
-('Jeff', 'Bezos', 11300),
+('Jeff', 'Bezos', 11469.87),
 ('Bill', 'Gates', 9800),
 
 ('Larry', 'Lässig', 248.2),
-('Mike', 'Mittellos', -169.87)
+('Mike', 'Mittellos', 30.13)
+;
+
+INSERT INTO transactions (account_from, account_to, amount)
+VALUES
+(NULL, 1, 15560),
+(NULL, 2, 11300),
+(NULL, 3, 9600),
+(NULL, 4, 448.2),
+(NULL, 5, 200),
+(5, 2, 169.87),
+(4, 3, 200)
 ;
